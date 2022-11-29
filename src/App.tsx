fae1a23d2388a1291bdf706d6fff3fc4b5044ef8
Mikah <mikahvaarama@gmail.com>
2022-11-29 19:17:51 +0100

    previous webapp transferred

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.scss';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import HomeView from './views/HomeView';
 import CategoriesView from './views/CategoriesView';
 import ProductsView from './views/ProductsView';
@@ -12,11 +13,34 @@ import ShoppingcartView from './views/ShoppingcartView';
 import NotFoundView from './views/NotFoundView';
 import { ProductsContext } from './contexts/contexts'
 
+
 function App() {
 
-  const products = null
-  const featured = null
-  const deals = null
+  const [products, setProducts] = useState([])
+  const [featured, setFeatured] = useState([])
+  const [deals, setDeals] = useState([])
+
+  useEffect(() => {
+    const fetchAllProducts = async () => {
+      let result = await fetch('https://win22-webapi.azurewebsites.net/api/products')
+      setProducts(await result.json())
+    }
+    fetchAllProducts()
+
+    const fetchFeaturedProducts = async () => {
+      let result = await fetch('https://win22-webapi.azurewebsites.net/api/products?take=8')
+      setFeatured(await result.json())
+    }
+    fetchFeaturedProducts()
+
+    const fetchDealsProducts = async () => {
+      let result = await fetch('https://win22-webapi.azurewebsites.net/api/products?take=4')
+      setDeals(await result.json())
+    }
+    fetchDealsProducts()
+
+  }, [setProducts, setFeatured, setDeals])  
+
 
   return (
     <BrowserRouter>
